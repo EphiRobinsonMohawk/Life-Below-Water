cb51b4332811fc8b36cc009d09732c19b87519d2
Ephi Robinson <000969115@mohawkcollege.ca>
2026-03-05 02:46:03 -0500

    Gyro

--- SubGamePrototypes/Assets/GyroScript.cs
+++ SubGamePrototypes/Assets/GyroScript.cs
@@ -7,15 +7,29 @@ public class GyroScript : MonoBehaviour
 
     void Update()
     {
-        if (ROVSub != null && GyroSub != null)
-        {
-            Vector3 rovRotation = ROVSub.transform.localEulerAngles;
-            Vector3 gyroRotation = GyroSub.transform.localEulerAngles;
+        if (ROVSub == null) return;
+        if (GyroSub == null) return;
 
-            gyroRotation.x = rovRotation.x;
-            gyroRotation.z = rovRotation.z;
+        Quaternion rov = ROVSub.transform.localRotation;
+        Quaternion gyro = GyroSub.transform.localRotation;
 
-            GyroSub.transform.localEulerAngles = gyroRotation;
-        }
+        Quaternion rovYaw = TwistAroundAxis(rov, Vector3.up);
+        Quaternion rovTilt = rov * Quaternion.Inverse(rovYaw);
+
+        Quaternion gyroYaw = TwistAroundAxis(gyro, Vector3.up);
+
+        GyroSub.transform.localRotation = rovTilt * gyroYaw;
     }
+
+    Quaternion TwistAroundAxis(Quaternion q, Vector3 axis)
+    {
+        axis.Normalize();
+        Vector3 v = new Vector3(q.x, q.y, q.z);
+        float d = Vector3.Dot(v, axis);
+        Quaternion twist = new Quaternion(axis.x * d, axis.y * d, axis.z * d, q.w);
+        return Quaternion.Normalize(twist);
+    }
+
+    //Twists fix the face switching issue when over 180 degrees, I don't get them ngl I got this fix from georgina
+
 }
