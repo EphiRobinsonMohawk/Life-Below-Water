25d4833d4ad737bd14354771adc1062bddebfc42
Ephi Robinson <000969115@mohawkcollege.ca>
2026-03-11 09:48:41 -0400

    Salp

---
+++ SubGamePrototypes/Assets/Salp/ChainChild.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ChainChild : MonoBehaviour
+{
+    public Transform chainIn;
+    public Transform chainOut;
+}
---
+++ SubGamePrototypes/Assets/Salp/ChainSpawner.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class ChainSpawner : MonoBehaviour
+{
+    public ChainChild salpPrefab;
+    public SalpFloater salpMove;
+    public int chainLength = 14;
+    public float minXRotation = 6f;
+    public float maxXRotation = 22f;
+
+    private void Awake()
+    {
+        chainLength = (Random.Range(11, 23));
+        BuildChain();
+    }
+    
+
+    void BuildChain()
+    {
+        if (salpPrefab == null) return;
+
+        ChainChild previous = null;
+
+        for (int i = 0; i < chainLength; i++)
+        {
+            ChainChild salp = Instantiate(salpPrefab, transform.position, transform.rotation);
+            salpMove.AddSalp(salp.gameObject);
+
+            if (previous != null)
+            {
+                salp.transform.rotation = previous.chainOut.rotation;
+                salp.transform.Rotate(Random.Range(minXRotation, maxXRotation), 0f, 0f, Space.Self);
+
+                Vector3 offset = previous.chainOut.position - salp.chainIn.position;
+                salp.transform.position += offset;
+            }
+
+            previous = salp;
+        }
+    }
+}
