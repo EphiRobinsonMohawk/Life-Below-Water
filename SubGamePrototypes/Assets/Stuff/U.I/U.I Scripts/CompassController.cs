0db6d2d88d6b22f11fbc523dcba13637e796a052
Stephan <wearshark13@gmail.com>
2026-03-16 14:02:42 -0400

    compassUpdated

--- SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/CompassController.cs
+++ SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/CompassController.cs
@@ -6,17 +6,18 @@ public class CompassController : MonoBehaviour
     public RectTransform compassContent;
 
     public float pixelsPerDegree = 5f;
-
-    float smoothX;
+    public float rulerWidth = 2000f;
 
     void Update()
     {
         float yaw = player.eulerAngles.y;
-        float target = -yaw * pixelsPerDegree;
 
-        smoothX = Mathf.Lerp(smoothX, target, Time.deltaTime * 5f);
+        float offset = yaw * pixelsPerDegree;
+
+        // Wrap position so ruler loops infinitely
+        float wrappedOffset = offset % rulerWidth;
 
         compassContent.anchoredPosition =
-            new Vector2(smoothX, compassContent.anchoredPosition.y);
+            new Vector2(-wrappedOffset, compassContent.anchoredPosition.y);
     }
 }
