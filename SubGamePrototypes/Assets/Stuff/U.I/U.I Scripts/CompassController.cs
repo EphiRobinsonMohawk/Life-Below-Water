4e7b7fee6f54d8118dde02e1847f9b8d04970851
Haedrich, Owen <000968723@mohawkcollege.ca>
2026-03-30 15:52:49 -0400

    Update compass.

--- SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/CompassController.cs
+++ SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/CompassController.cs
@@ -5,14 +5,13 @@ public class CompassController : MonoBehaviour
     public Transform player;
     public RectTransform compassContent;
 
-    public float pixelsPerDegree = 5f;
     public float rulerWidth = 2000f;
 
     void Update()
     {
         float yaw = player.eulerAngles.y;
 
-        float offset = yaw * pixelsPerDegree;
+        float offset = yaw * rulerWidth/360;
 
         // Wrap position so ruler loops infinitely
         float wrappedOffset = offset % rulerWidth;
