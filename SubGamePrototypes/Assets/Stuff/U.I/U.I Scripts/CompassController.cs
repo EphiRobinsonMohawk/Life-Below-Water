9c801ca3858eafb016ceb574454b4c6cc5b7db78
Haedrich, Owen <000968723@mohawkcollege.ca>
2026-03-31 09:21:04 -0400

    Add compass boat icon

--- SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/CompassController.cs
+++ SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/CompassController.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class CompassController : MonoBehaviour
 {
     public Transform player;
+    public Transform homeBase;
     public RectTransform compassContent;
+    public RectTransform homeBaseMarker;
 
     public float rulerWidth = 2000f;
 
@@ -11,12 +13,39 @@ public class CompassController : MonoBehaviour
     {
         float yaw = player.eulerAngles.y;
 
-        float offset = yaw * rulerWidth/360;
+        float offset = yaw * rulerWidth / 360;
 
         // Wrap position so ruler loops infinitely
         float wrappedOffset = offset % rulerWidth;
 
         compassContent.anchoredPosition =
             new Vector2(-wrappedOffset, compassContent.anchoredPosition.y);
+
+        // Update home base marker
+        if (homeBase != null && homeBaseMarker != null)
+        {
+            Vector3 directionToHome = homeBase.position - player.position;
+            directionToHome.y = 0;
+
+            if (directionToHome.sqrMagnitude > 0.001f)
+            {
+                if (!homeBaseMarker.gameObject.activeSelf) homeBaseMarker.gameObject.SetActive(true);
+
+                float angleToHome = Mathf.Atan2(directionToHome.x, directionToHome.z) * Mathf.Rad2Deg;
+                float relativeAngle = Mathf.DeltaAngle(yaw, angleToHome);
+
+                float xPos = relativeAngle * (rulerWidth / 360f);
+                homeBaseMarker.anchoredPosition = new Vector2(xPos, homeBaseMarker.anchoredPosition.y);
+            }
+            else
+            {
+                // We are at home base
+                homeBaseMarker.gameObject.SetActive(false);
+            }
+        }
+        else if (homeBaseMarker != null && homeBase == null)
+        {
+            homeBaseMarker.gameObject.SetActive(false);
+        }
     }
 }
