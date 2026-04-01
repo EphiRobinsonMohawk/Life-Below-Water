983bb134787d2a992a8976e06fe4e6e719562b07
Stephan <wearshark13@gmail.com>
2026-04-01 12:12:46 -0400

    speedometerlevels

--- SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/SpeedometerLevels.cs
+++ SubGamePrototypes/Assets/Stuff/U.I/U.I Scripts/SpeedometerLevels.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeedometerLevels : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private Speedometer speedometerScript;
+
+    [SerializeField] private Image Speedlevel_1;
+    [SerializeField] private Image Speedlevel_2;
+    [SerializeField] private Image Speedlevel_3;
+
+
+
+    private void Start()
     {
-        
+        if (Speedlevel_1 != null) Speedlevel_1.enabled = false;
+        if (Speedlevel_2 != null) Speedlevel_2.enabled = false;
+        if (Speedlevel_3 != null) Speedlevel_3.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+        if (speedometerScript != null)
+        {
+            float currentSpeed = speedometerScript.maxSpeed;
+
+
+            if (currentSpeed > 15f)
+            {
+                if (Speedlevel_1 != null) Speedlevel_1.gameObject.SetActive(true);
+            }
+            if (currentSpeed < 15f)
+            {
+                if (Speedlevel_1 != null) Speedlevel_1.gameObject.SetActive(false);
+            }
+            if (currentSpeed > 40f && currentSpeed <= 130f)
+            {
+                if (Speedlevel_2 != null) Speedlevel_2.gameObject.SetActive(true);
+            }
+            if (currentSpeed < 40f)
+            {
+                if (Speedlevel_2 != null) Speedlevel_3.gameObject.SetActive(true);
+
+            }
+            if (currentSpeed > 80f)
+            {
+                if (Speedlevel_3 != null) Speedlevel_3.gameObject.SetActive(true);
+            }
+            if (currentSpeed < 80f)
+            {
+                if (Speedlevel_3 != null) Speedlevel_3.gameObject.SetActive(false);
+            }
+        }
+
+
     }
+
 }
